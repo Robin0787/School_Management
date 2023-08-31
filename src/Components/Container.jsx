1570f0e6ab1fa833cfa010f9c7c42962d861ab23
Robin <mohammadrobin636@gmail.com>
2023-08-31 15:45:56 +0600

    commit

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,7 +1,7 @@
 
 const Container = ({children}) => {
     return (
-        <section className='w-[95%] xl:w-[90%] 2xl:w-[80%] mx-auto'>
+        <section className='w-[95%] xl:w-[90%] 2xl:w-[85%] mx-auto'>
             {children}
         </section>
     );
